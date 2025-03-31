7a9748fdb1b76674313e93c822f94335058f20b0
bora <borado.lee7@gmail.com>
2025-03-31 17:39:05 +0900

    refactor : item-qty-changer

--- components/cart/item-qty-changer.tsx
+++ components/cart/item-qty-changer.tsx
@@ -2,7 +2,7 @@
 import { Minus, Plus } from "lucide-react";
 import Flex from "../common/flex";
 import { Button } from "../ui/button";
-import { CartItem } from "@/types";
+import { CartItemType } from "@/types";
 import { useTransition } from "react";
 import {
   modifyItemQtyToCart,
@@ -10,59 +10,46 @@ import {
 } from "@/lib/actions/cart.actions";
 import { toast } from "@/hooks/use-toast";
 
+const TYPE = ["minus", "qty", "plus"];
 const ItemQtyChanger = ({
   item,
   isView,
 }: {
-  item: CartItem;
+  item: CartItemType;
   isView?: boolean;
 }) => {
   const [isPending, startTransition] = useTransition();
   const handleItemQty = (type: string) => {
     const qty = type === "plus" ? item.qty + 1 : item.qty - 1;
     startTransition(async () => {
-      if (qty) {
-        //modify
-        const { success, message } = await modifyItemQtyToCart(item, qty);
-        if (!success) {
-          toast({ variant: "destructive", description: message });
-        } else {
-          toast({
-            description: message,
-          });
-        }
-      } else {
-        // remove
-        const { success, message } = await removeItemToCart(item);
-        if (!success) {
-          toast({ variant: "destructive", description: message });
-        } else {
-          toast({
-            description: message,
-          });
-        }
-      }
+      const { success, message } = qty
+        ? await modifyItemQtyToCart(item, qty)
+        : await removeItemToCart(item);
+      toast({
+        variant: success ? "default" : "destructive",
+        description: message,
+      });
     });
   };
   return (
     <Flex className="justify-between">
-      <Button
-        className={isView ? "hidden" : "block"}
-        variant="outline"
-        onClick={() => handleItemQty("minus")}
-        disabled={isPending}
-      >
-        <Minus />
-      </Button>
-      <span className="w-12 text-center">{item.qty}</span>
-      <Button
-        className={isView ? "hidden" : "block"}
-        variant="outline"
-        onClick={() => handleItemQty("plus")}
-        disabled={isPending}
-      >
-        <Plus />
-      </Button>
+      {TYPE.map((type) =>
+        type === "qty" ? (
+          <span key={type} className="w-12 text-center">
+            {item.qty}
+          </span>
+        ) : (
+          <Button
+            key={type}
+            className={isView ? "hidden" : "block"}
+            variant="outline"
+            onClick={() => handleItemQty(type)}
+            disabled={isPending}
+          >
+            {type === "plus" ? <Plus /> : <Minus />}
+          </Button>
+        )
+      )}
     </Flex>
   );
 };

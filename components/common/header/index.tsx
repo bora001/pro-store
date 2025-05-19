e5c74fd8b08a9cd386b318e524bb8cf5d37967c1
bora <borado.lee7@gmail.com>
2025-05-19 14:34:17 +0900

    feat(autocomplete): support keyboard navigation

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -8,19 +8,11 @@ import SearchInput from "../search-input";
 import CategoryDrawer from "./menu/category-drawer";
 import { getAllCategory } from "@/lib/actions/handler/home.actions";
 
-const Header = async ({
-  navList,
-  isAdmin,
-}: {
-  navList?: NavBarType;
-  isAdmin?: boolean;
-}) => {
+const Header = async ({ navList, isAdmin }: { navList?: NavBarType; isAdmin?: boolean }) => {
   const { data: category } = await getAllCategory();
   return (
     <header
-      style={{
-        height: `${CONSTANTS.HEADER_HEIGHT}px`,
-      }}
+      style={{ height: `${CONSTANTS.HEADER_HEIGHT}px` }}
       className={`w-full border-b fixed z-50 bg-white/85 backdrop-blur dark:bg-black/85 dark:border-gray-800`}
     >
       <div className="wrapper flex-between">
@@ -29,9 +21,7 @@ const Header = async ({
           <CategoryDrawer category={category || []} />
           <Link href={PATH.HOME} className="flex-start">
             <LogoImage />
-            <span className="hidden lg:block font-bold text-2xl ml-3">
-              {CONFIG.APP_NAME}
-            </span>
+            <span className="hidden lg:block font-bold text-2xl ml-3">{CONFIG.APP_NAME}</span>
           </Link>
           {navList && <NavBar navList={navList} />}
         </div>
